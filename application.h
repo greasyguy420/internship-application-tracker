f8e66f8c093bcec806fa77069d5262e3bd7d9bc3
Joshua <js8111@gmail.com>
2026-02-14 18:15:48 -0500

    Separate the header file from the function definitions

--- application.h
+++ application.h
@@ -1,4 +1,6 @@
-#include <iostream>
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
 #include <string>
 using namespace std;
 
@@ -30,44 +32,4 @@ class Application{
         string notes;
 };
 
-Application::Application(){
-    company = "Unkown Company";
-    role = "Unkown Role";
-    location = "TBD";
-    status = "Not Applied";
-    date_applied = "N/A";
-    notes = "No notes";
-};
-
-void Application::display() const{
-    cout << company;
-    cout << role;
-    cout << location;
-    cout << status;
-    cout << date_applied;
-    cout << notes;
-};
-
-void Application::updateCompany(const string& newCompany){
-    company = newCompany;
-};
-
-void Application::updateRole(const string& newRole){
-    role = newRole;
-};
-
-void Application::updateLocation(const string& newLocation){
-    location = newLocation;
-};
-
-void Application::updateStatus(const string& newStatus){
-    status = newStatus;
-};
-
-void Application::updateDate(const string& newDate){
-    date_applied = newDate;
-};
-
-void Application::updateNotes(const string& newNotes){
-    notes = newNotes;
-};
+#endif
