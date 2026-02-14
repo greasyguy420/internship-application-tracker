d36b8b0df967b5baa79e0267049888124e747371
Joshua <js8111@gmail.com>
2026-02-14 18:07:00 -0500

    Implement Getter Functions

--- application.h
+++ application.h
@@ -6,12 +6,20 @@ class Application{
     public:
         Application();
         void display() const;
+
         void updateCompany(const string& newCompany);
         void updateRole(const string& newRole);
         void updateLocation(const string& newLocation);
         void updateStatus(const string& newStatus);
         void updateDate(const string& newDate);
         void updateNotes(const string& newNotes);
+
+        string getCompany() const{return company;}
+        string getRole() const{return role;}
+        string getLocation() const{return location;}
+        string getStatus() const{return status;}
+        string getDate() const{return date_applied;}
+        string getNotes() const{return notes;}
 
     private:
         string company;
